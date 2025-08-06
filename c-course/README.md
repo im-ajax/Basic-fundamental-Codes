f23812080cd0977236da1c1685b6cdc05464b6fe
im-ajax <saniahmed0480@gmail.com>
2025-08-07 01:04:07 +0600

    added many files

---
+++ c-course/README.md
@@ -0,0 +1,20 @@
+#Things to do before starting coding in c
+
+there is a file named configuration provided in the folder.
+name -> configuration.txt
+
+in your vs code, you have to install an extension named code runner. then go to the settings and search for executor map and then copy all the texts from the configuration file and paste it into the (settings.json) and hit ctrl + s (if you dont have the autosave feature).
+
+you need to get vs code installed and gitbash according to your computer <br>
+if your computer is x64 or x86 then download it according to your device.
+
+<bold>if you dont give a attention while installing the git according to your device, you will not be able to get the output of the code.</bold>
+
+then just set-up the <bold>mingw</bold> file and start coding
+
+after the setup check 
+win+run = type "cmd" then hit enter
+and check 
+-> git --version (in the terminal)
+
+if all set you are ready to code.
--- c-course/basic_operations.c
+++ c-course/basic_operations.c
@@ -1,8 +1,53 @@
+// Datatypes and variables ->
+    /*lets start with datatypes and variables
+      int -> for integer 
+      float -> float values
+      long long -> for bigger integers
+      double/long float -> for bigger float values
+
+      format specifier:
+      int -> %d
+      float -> %f
+      long long int/long long -> %lld
+      long float/double -> %lf
+    */
+
+    // variables ->
+    /*
+        for variables we can choose any type of meaningful names
+        but there are some rules :
+
+        prohibitions :
+        -> variables cannot start with numbers : 12variable
+        -> cannot start with special characters : #variable
+        -> cannot contain spaces : this is a variable
+        -> cannot be in quotation : "variable name"
+
+        rules:
+        -> start with mixed order letters : variable/Variable
+        -> user underscode instead of spaces : variable_name
+        -> if needed to add numbers then add in last : variable1,variable2
+        -> you can mix uppercase lowercase : varialbe,VaRiable
+    */
+
+
+
+// basic datatype
+
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
 int main(){
-
+    //declaring a new variable
+    int a;
+    // assigning a value 
+    a=10;
+    // printing 
+    //for printing we use printf("Format specifier",variable of that corresponding specifier);
+    printf("%d",a);
+    //here %d holds a value of a. so we are giving the format specifier and that is holding a value of a
+    // so that we can get value of a 
+    // final : %d came from the ancestors we use 
     return 0;
 }
